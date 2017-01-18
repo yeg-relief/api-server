15564a47f55b522f4abdf92dcfde01e9e9fd56ef
Steven Myers <slmyers@ualberta.ca>
2017-01-18 02:47:03 -0700

    fixing up i hope

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function test(n) {
-    return n + 1;
-}
-console.log(test(2));
--- build/models/condition/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./boolean-condition"));
-__export(require("./number-condition"));
-__export(require("./program-condition"));
