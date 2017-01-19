00bb158e02a90527e2672cc15c0f80bcd9a35814
Steven Myers <slmyers@ualberta.ca>
2017-01-18 17:39:46 -0700

    commit before downtown

--- app/models/question/expandable-question.ts
+++ app/models/question/expandable-question.ts
@@ -1,6 +1,7 @@
 import { IExpandableQuestion, INumberRadio, IBooleanRadio, INumberInput, ConcreteQuestion, IQuestion } from '../../interfaces';
 import { NumberInputQuestion } from './input-question'
 import { NumberRadioQuestion, BooleanRadioQuestion } from './radio-question';
+import { Question } from './question';
 
 export class ExpandableQuestion extends BooleanRadioQuestion implements IExpandableQuestion {
   expandable: boolean;
@@ -10,20 +11,58 @@ export class ExpandableQuestion extends BooleanRadioQuestion implements IExpanda
     super(question);
     const q = <IExpandableQuestion>question;
     this.expandable = q.expandable;
-    this.conditonalQuestions = this.constructConditionals(q.conditonalQuestions);
+    this.conditonalQuestions = constructConcreteQuestions(q.conditonalQuestions);
   }
 
-  private 
-  constructConditionals(question: ConcreteQuestion[]): ConcreteQuestion[]
-  {
-    return 
-  }
 
   validate(): boolean {
-    for(let i = 0; i < this.conditonalQuestions.length; i++){
-      
+    for (let i = 0; i < this.conditonalQuestions.length; i++) {
+      if(!validateFn(this.conditonalQuestions[i])){
+        return false;
+      }
     }
 
-    return this.validateBooleanQuestion() && this.expandable === true; 
+    return this.validateBooleanQuestion() && this.expandable === true;
   }
+}
+
+function constructConcreteQuestion(question: IQuestion): ConcreteQuestion {
+  let concreteQuestion;
+  concreteQuestion = new BooleanRadioQuestion(question);
+  if(validateFn(concreteQuestion)){
+    return concreteQuestion;
+  }
+
+  concreteQuestion = new NumberRadioQuestion(question);
+  if(validateFn(concreteQuestion)){
+    return concreteQuestion;
+  }
+
+  concreteQuestion = new NumberInputQuestion(question)
+  if(validateFn(concreteQuestion)){
+    return concreteQuestion;
+  }
+
+  return undefined;
+}
+
+function validateFn(question: ConcreteQuestion): boolean {
+  try {
+    if (question.validate()) {
+      return true;
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  return false;
+}
+
+
+function constructConcreteQuestions(questions: IQuestion[]): ConcreteQuestion[] {
+  const concreteQuestions: ConcreteQuestion[] = questions.map(q => constructConcreteQuestion(q));
+  const undef = concreteQuestions.find(q => q === undefined);
+  if (undef !== undefined){
+    throw new Error('an invalid question!');
+  }
+  return concreteQuestions;
 }
