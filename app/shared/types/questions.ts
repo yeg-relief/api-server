6bca02ee4cc6be0068132faae893ded07173251f
Steven Myers <slmyers@ualberta.ca>
2017-09-09 21:53:23 -0600

    [mult-select-question] intial commit (#12)

--- app/shared/types/questions.ts
+++ app/shared/types/questions.ts
@@ -1,43 +1,54 @@
 import { BooleanOption, NumberOption } from './index';
 
-export type Question = ( NumberInput | BooleanRadio | NumberRadio | ExpandableQuestion );
+export type Question = ( NumberInput | BooleanRadio | NumberRadio | ExpandableQuestion | Multiselect);
 
 type id = string;
 
 export type NumberInput = {
-  id: string;
-  label: string;
-  key: string;
-  controlType: 'NumberInput';
-  value?: number;
-  index: number;
+    id: string;
+    label: string;
+    key: string;
+    controlType: 'NumberInput';
+    value?: number;
+    index: number;
 }
 
 export type BooleanRadio = {
-  id: string;
-  label: string;
-  key: string;
-  controlType: 'Toggle';
-  options: BooleanOption[];
-  index: number;
+    id: string;
+    label: string;
+    key: string;
+    controlType: 'Toggle';
+    options: BooleanOption[];
+    index: number;
 }
 
 export type NumberRadio = {
-  id: string;
-  label: string;
-  key: string;
-  controlType: 'NumberSelect';
-  options: NumberOption[];
-  index: number;
+    id: string;
+    label: string;
+    key: string;
+    controlType: 'NumberSelect';
+    options: NumberOption[];
+    index: number;
 }
 
 export type ExpandableQuestion = {
-  id: string;
-  expandable: boolean;
-  label: string;
-  key: string;
-  controlType: 'Toggle';
-  options: BooleanOption[];
-  conditonalQuestions: id[];
-  index: number;
+    id: string;
+    expandable: boolean;
+    label: string;
+    key: string;
+    controlType: 'Toggle';
+    options: BooleanOption[];
+    conditonalQuestions: id[];
+    index: number;
+}
+
+export type Multiselect = {
+    id: string;
+    expandable: boolean;
+    label: string;
+    key: string;
+    controlType: 'Multiselect';
+    options: any[];
+    conditonalQuestions: id[];
+    index: number;
 }
