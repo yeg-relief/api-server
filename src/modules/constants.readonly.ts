3485ee0b31f25c98af53d2c4f1deaacdcce46998
Steven Myers <slmyers@ualberta.ca>
2018-03-16 16:31:50 -0600

    integration with docker-compose

--- src/modules/constants.readonly.ts
+++ src/modules/constants.readonly.ts
@@ -1,9 +1,13 @@
 const determineHost = () => {
+    return "elasticsearch:9200";
+    /*
+
     if (process.env.NODE_ENV === "INTEGRATION_TEST") return "http://localhost:9400";
 
     if (process.env.COMPOSER_ENV === "true") return `elasticsearch:${process.env.ELASTICSEARCH_PORT}`;
 
     return "http://localhost:9200"
+    */
 };
 
 
