b2cae10427b6da1198fe714ff4d4c52507534a4b
Steven Myers <slmyers@ualberta.ca>
2018-03-16 17:00:45 -0600

    working on composer

--- src/modules/constants.readonly.ts
+++ src/modules/constants.readonly.ts
@@ -1,19 +1,15 @@
 const determineHost = () => {
-    return "elasticsearch:9200";
-    /*
-
     if (process.env.NODE_ENV === "INTEGRATION_TEST") return "http://localhost:9400";
 
     if (process.env.COMPOSER_ENV === "true") return `elasticsearch:${process.env.ELASTICSEARCH_PORT}`;
 
     return "http://localhost:9200"
-    */
 };
 
 
 export class ConstantsReadonly {
     readonly host:string = determineHost();
-    readonly logLevel:string = 'trace';
+    readonly logLevel:string = process.env.COMPOSER_ENV === "true"? '' : 'trace';
     private readonly _domain:string = "EDMONTON";
     readonly domain:string = process.env.NODE_ENV === "development" ? "devel" : this._domain
 }
